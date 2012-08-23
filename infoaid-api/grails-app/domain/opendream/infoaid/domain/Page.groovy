a9190885e5441f304dc1b5a34b5441e34d4b40c3
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-08-23 16:04:21 +0700

    add slug property

--- infoaid-api/grails-app/domain/opendream/infoaid/domain/Page.groovy
+++ infoaid-api/grails-app/domain/opendream/infoaid/domain/Page.groovy
@@ -1,18 +1,32 @@
 package opendream.infoaid.domain
 
 class Page {
+    def slugGeneratorService
+
     String name
     String lat
     String lng
     Date dateCreated
     Date lastUpdated
     Location location
+    String slug = ""
 
     static hasMany = [posts:Post]
+    static transients = ['slugGeneratorService']
 
     static constraints = {
         lat nullable:true
         lng nullable:true
         location nullable:true, unique:false
+    }
+
+    def beforeInsert() {
+        this.slug = slugGeneratorService.generateSlug(this.class, "slug", name)
+    }
+
+    def beforeUpdate() {
+        if (isDirty('name')) {
+            this.slug = slugGeneratorService.generateSlug(this.class, "slug", name)
+        }
     }
 }
