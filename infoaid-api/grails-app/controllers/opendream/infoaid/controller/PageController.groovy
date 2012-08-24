78fb78a77bf7b363b48a45688c484cd5cef30178
Nachapol Tongyusook <nachapol.t@gmail.com>
2012-08-24 17:23:46 +0700

    pageController test

--- infoaid-api/grails-app/controllers/opendream/infoaid/controller/PageController.groovy
+++ infoaid-api/grails-app/controllers/opendream/infoaid/controller/PageController.groovy
@@ -1,6 +1,22 @@
 package opendream.infoaid.controller
 
+import grails.converters.JSON
+
 class PageController {
+    def pageService
 
     def index() { }
+
+    def info() {
+        def info = pageService.getInfo(params.pageId)
+        def ret = [id: info.id, name: info.name, lat: info.lat, lng: info.lng, dateCreated: info.dateCreated.format('yyyy-MM-dd HH:mm'), 
+        lastUpdated: info.lastUpdated.format('yyyy-MM-dd HH:mm')
+        ]
+
+        render ret as JSON
+    }
+
+    def needs() {
+        def needs = pageService.getNeeds()
+    }
 }
--- infoaid-api/test/unit/opendream/infoaid/controller/PageControllerTests.groovy
+++ infoaid-api/test/unit/opendream/infoaid/controller/PageControllerTests.groovy
@@ -5,12 +5,39 @@ package opendream.infoaid.controller
 import grails.test.mixin.*
 import org.junit.*
 import opendream.infoaid.domain.Page
+import opendream.infoaid.service.PageService
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(PageController)
-@Mock([Page])
+@Mock([Page, PageService])
 class PageControllerTests {
+    def pageService
 
+    void testInfo() {
+        def date = new Date()
+        def dateStr = date.toString()
+        Page.metaClass.generateSlug = {-> 'slug'}
+        Page.metaClass.isDirty = {name -> false}
+
+        new Page(id: 1, name: "page", lat: "111", lng: "222", dateCreated: date, lastUpdated: date).save()
+        pageService = mockFor(PageService)
+
+        pageService.demand.getInfo(1..2) {id -> Page.get(id)}
+
+        controller.pageService = pageService.createMock()
+        params.pageId = 1
+        def dateFormat = "yyyy-MM-dd HH:mm"
+        def expectResponse = """{"id":1,"name":"page","lat":"111","lng":"222","dateCreated":"${date.format(dateFormat)}","lastUpdated":"${date.format(dateFormat)}"}"""
+        controller.info()
+
+        assert expectResponse == response.text
+
+        params.pageId = 2
+        controller.info()
+        assert response.text == null
+    }
+
+    
 }
