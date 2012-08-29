1d7666d14d6b2fe565e1d426219b2fb5bc6758a1
Nachapol Tongyusook <nachapol.t@gmail.com>
2012-08-29 14:03:40 +0700

    add urlMapping : limitNeed, need, about

--- infoaid-api/grails-app/conf/UrlMappings.groovy
+++ infoaid-api/grails-app/conf/UrlMappings.groovy
@@ -10,6 +10,9 @@ class UrlMappings {
         "/$slug/members"(controller: "page", action: "member")  
         "/$slug/status"(controller: "page", action: "status")
         "/$slug/map"(controller: "page", action: "map")
+        "/$slug/need"(controller: "page", action: "need")
+        "/$slug/limitNeed/$limit"(controller: "page", action: "limitNeed")
+        "/$slug/about"(controller: "page", action: "about")
 		"/"(view:"/index")
 		"500"(view:'/error')
 	}
