fb3a439a9230f5e9271df1b43827210070a7c262
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-08-31 12:01:59 +0700

    implement getBasicInfo

--- infoaid-api/grails-app/services/opendream/infoaid/service/UserService.groovy
+++ infoaid-api/grails-app/services/opendream/infoaid/service/UserService.groovy
@@ -6,11 +6,16 @@ class UserService {
 
     def create(userparams) {
         def user = new User()
-        user.properties['username', 'password', 'firstname', 'lastname', 'dateCreated', 'lastUpdated'] = userparams
+        user.properties['username', 'password', 'firstname', 'lastname', 'email', 'telNo', 'dateCreated', 'lastUpdated'] = userparams
         if(!user.save()) {
             log.error user.errors
             throw RuntimeException("${user.errors}")
         }
         user
+    }
+
+    def getBasicInfo(userId) {
+        def user = User.get(userId)
+        [username:user.username, firstname:user.firstname, lastname:user.lastname, email:user.email]
     }
 }
