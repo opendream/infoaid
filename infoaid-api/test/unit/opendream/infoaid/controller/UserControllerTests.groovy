c844730f476cee10faa30b0ab55a4f52a1d008a5
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-08-31 17:00:58 +0700

    create testShowBasicInfo

--- infoaid-api/test/unit/opendream/infoaid/controller/UserControllerTests.groovy
+++ infoaid-api/test/unit/opendream/infoaid/controller/UserControllerTests.groovy
@@ -1,6 +1,7 @@
 package opendream.infoaid.controller
 
 import opendream.infoaid.domain.User
+import opendream.infoaid.service.UserService
 
 import grails.test.mixin.*
 import org.junit.*
@@ -12,6 +13,7 @@ import org.junit.*
 @Mock([User])
 class UserControllerTests {
     def user
+    def userService
 
     @Before
     void setup() {
@@ -22,12 +24,16 @@ class UserControllerTests {
             firstname: 'thawatchai', lastname: 'jong', dateCreated: new Date(), 
             lastUpdated: new Date()).save(flush:true)
         
-        controller.userService = [create: { mockparams -> def newuser = new User()
-                                        newuser.properties = mockparams 
-                                        newuser.save() } ]
+        userService = mockFor(UserService)
+        //controller.userService = [create: { mockparams -> def newuser = new User()
+                                        //newuser.properties = mockparams 
+                                        //newuser.save() } ]
     }
 
     void testCreate() {
+        userService.demand.create(1..1) { mockparams -> def newuser = new User(mockparams).save() }
+        controller.userService = userService.createMock()
+
         params.username = "nut"
         params.password = "nut"
         params.firstname = 'firstname'
@@ -41,7 +47,7 @@ class UserControllerTests {
     }
 
     void testCreateFail() {
-        controller.userService = [create: { mockparams -> throw RuntimeException("errors") } ]
+        controller.userService = [create: { mockparams -> throw RuntimeException("errors") }]
 
         params.username = "nut"
         params.password = "nut"
@@ -49,9 +55,24 @@ class UserControllerTests {
         params.lastname = 'lastname'
         params.dateCreated = new Date()
         params.lastUpdated = new Date()
+
         controller.create()
+
         assert 1 == User.count()
         assert 'can not create new user' == response.json.message
         assert 'nut' == response.json.user.username
+    }
+
+    void testShowBasicInfo() {        
+        userService.demand.getBasicInfo(1..1) { userId -> [username:user.username, firstname:user.firstname, 
+                                                            lastname:user.lastname, email:user.email] } 
+        controller.userService = userService.createMock()
+
+        params.userId = user.id
+        controller.showBasicInfo()
+
+        assert 1 == User.count()
+        assert 'admin' == response.json.username
+        assert 'jong' == response.json.lastname
     }
 }
