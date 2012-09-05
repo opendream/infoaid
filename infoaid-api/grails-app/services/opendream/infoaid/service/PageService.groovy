fb736403e55e84a9c6e8f929fc347b9cce549118
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-09-05 12:54:15 +0700

    add failOnError: true to page.save()

--- infoaid-api/grails-app/services/opendream/infoaid/service/PageService.groovy
+++ infoaid-api/grails-app/services/opendream/infoaid/service/PageService.groovy
@@ -91,10 +91,7 @@ class PageService {
     def createPage(userId, name, lat, lng, location, household, population, about) {
         def page = new Page(name: name, lat: lat, lng: lng, location: location,
             household: household, population: population, about: about)
-        if(!page.save()) {
-            //throw new RuntimeException("can not save new page")
-            return false
-        }
+        page.save(failOnError: true)
         def user = User.get(userId)
         PageUser.createPage(user, page)
         page
