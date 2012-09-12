79d43065ead8329371a78415e714b82a2552ab94
Nachapol Tongyusook <nachapol.t@gmail.com>
2012-09-12 17:50:38 +0700

    fix api

--- infoaid-api/grails-app/services/opendream/infoaid/service/PageService.groovy
+++ infoaid-api/grails-app/services/opendream/infoaid/service/PageService.groovy
@@ -12,16 +12,14 @@ import opendream.infoaid.domain.MessagePost
 class PageService {
 
     def getInfo(slug) {
-
     	def pageInfo = Page.findBySlug(slug)
     }
 
-    def getPosts(slug, fromId=null, toId=null, since=null, until=null, type = null) {
-        def max = 10
+    def getPosts(slug, fromId=null, toId=null, since=null, until=null, max = 10, type = null) {
         def posts = Post.createCriteria().list() {
             eq('status', Post.Status.ACTIVE)
             page {
-                eq('slug', slug)    
+                eq('slug', slug)
             }
             maxResults(max)
             if(fromId) {
@@ -44,12 +42,12 @@ class PageService {
         return posts
     }
 
-    def getTopPost(slug, fromId=null, toId=null, since=null, until=null) {
-        getPosts(slug, fromId, toId, since, until, 'top')
+    def getTopPost(slug, fromId=null, toId=null, since=null, until=null, max = 10) {
+        getPosts(slug, fromId, toId, since, until, max, 'top')
     }
 
-    def getRecentPost(slug, fromId=null, toId=null, since=null, until=null) {
-        getPosts(slug, fromId, toId, since, until, 'recent')        
+    def getRecentPost(slug, fromId=null, toId=null, since=null, until=null, max = 10) {
+        getPosts(slug, fromId, toId, since, until, max, 'recent')
     }
 
     def getComments(postId, fromId=null, toId=null, since=null, until=null) {
