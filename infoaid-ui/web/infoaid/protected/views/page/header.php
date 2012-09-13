cdc8e365575582da246125252784f5650c392d3d
Nachapol Tongyusook <nachapol.t@gmail.com>
2012-09-13 17:12:43 +0700

    refactor code

--- infoaid-ui/web/infoaid/protected/views/page/header.php
+++ infoaid-ui/web/infoaid/protected/views/page/header.php
@@ -1,56 +1,74 @@
 <?php
     $slugEncode = urlencode($slug);
     $post = API::getJSON("page/$slugEncode/info");
+    $needs = API::getJSON('page/'.$slug.'/limit_need/4');
 ?>
 <div id="page-header-<?php echo $id; ?>" class='page-header'>
     <div id="page-header-left-<?php echo $id; ?>">
-        <div class="page-picture"></div>
+        <div class="page-picture">
+            <?php if($post->picSmall == null) {
+                echo "<img src=" .$baseUrl.$post->picSmall . "/>";
+                } else {
+                    echo '<img src="/infoaid/media/pages/page_default_small.jpg"/>';
+                }
+            ?>
+        </div>
     </div>
     <div id="page-header-right-<?php echo $id; ?>">
         <div><span class='page-name'><?php echo CHtml::link($post->name, array("page/$slug")); ?></span></div>
-        <div><span class='page-household'></span><span class='page-population'></span></div>
-        <div><span class='page-lat'></span><span class='page-lng'></span></div>
-        <div><span class='page-needs'>Need : </span></div>
+        <div class='page-household-population'>
+            <span>
+                <?php
+                    if($post->household == null) {
+                        echo '? House';
+                    } else {
+                        echo $post->household.' House';
+                    }
+                ?>
+            </span>
+            <span>
+                <?php
+                    if($post->population == null) {
+                        echo '? Man';
+                    } else {
+                        echo $post->population.' Man';
+                    }
+                ?>
+            </span>
+        </div>
+        <div class='page-lat-lng'>
+            <span>
+                <?php
+                    if($post->lat == null) {
+                        echo '<b>Latitude :</b> ?';
+                    } else {
+                        echo '<b>Latitude :</b> '.$post->lat;
+                    }
+                ?>
+            </span>
+            <span>
+                <?php
+                    if($post->lng == null) {
+                        echo '<b>Longitude :</b> ?';
+                    } else {
+                        echo '<b>Longitude :</b> '.$post->lng;
+                    }
+                ?>
+            </span>
+        </div>
+        <div>
+            <span class='page-needs'>
+                <b>Need :</b>
+                    <?php
+                        if($needs->status == 0) {
+                            echo ' ?';
+                        } else {
+                            foreach($needs->needs as $el) {
+                                echo ' '.$el->message.' ';
+                            }    
+                        }
+                    ?>
+            </span>
+        </div>
     </div>
 </div>
-<script>
-(function() {
-
-    var urlInfo = "<?php echo $this->createUrl('api/pageInfo'); ?>";
-    var urlNeeds = "<?php echo $this->createUrl('api/pageNeeds'); ?>";
-    var pageId = "#page-header-<?php echo $id; ?>";
-    var imgStr = ''
-    $.getJSON(urlInfo, {slug: "<?php echo $slug; ?>"}, function (resp) {
-        
-        if(resp.picSmall != null) {
-            imgStr = '<img src="' + baseUrl + resp.picSmall + '"/>';
-        } else {
-            imgStr = '<img src="' + baseUrl + '/media/pages/page_default_small.jpg"/>';
-        }
-
-        if(resp.household != null) {
-            $('.page-household', pageId).html(resp.household + ' house ');
-        } else {
-            $('.page-household', pageId).html(' ? house ');
-        }
-
-        if(resp.population != null) {
-            $('.page-population', pageId).html(resp.population + ' man');
-        } else {
-            $('.page-population', pageId).html(' ? man');
-        }
-        
-        $('.page-picture', pageId).html(imgStr);
-        
-        
-        $('.page-lat', pageId).html('Latitude : ' + resp.lat);
-        $('.page-lng', pageId).html(' Longitude : ' + resp.lng);
-    });
-
-    $.getJSON(urlNeeds, {slug: "<?php echo $slug ?>", limit: 4}, function (resp) {
-        for(el in resp.needs) {
-            $('.page-needs', pageId).append(' ' + resp.needs[el].message);
-        }
-    });
-})();
-</script>
