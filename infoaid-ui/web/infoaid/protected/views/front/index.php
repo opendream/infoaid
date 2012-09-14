d1b43b355e1e2936102db7ad07777aa70e423cac
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-09-14 14:37:03 +0700

    Make marker on front page map clickable to page view

--- infoaid-ui/web/infoaid/protected/views/front/index.php
+++ infoaid-ui/web/infoaid/protected/views/front/index.php
@@ -49,6 +49,9 @@ jQuery(function ($) {
 					return renderMarkerTooltip(item);
 				},
 				trigger: 'hover'
+			})
+			.click(function (evt) {
+				window.location.href = baseUrl + '/index.php/page/' + item.slug;
 			});
 	};
 
