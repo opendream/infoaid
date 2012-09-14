579bf1f1071526fbf836d31d9186d9fb9d7f3ad0
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-09-14 09:45:18 +0700

    Make mock server support for wildcard request

--- mock-server/server.js
+++ mock-server/server.js
@@ -3,6 +3,8 @@ var http = require('http'),
     url = require('url')
 ;
 
+var universal_pattern = /^\/(\w+)\/(?:\w|-)+\/(\w+)/;
+
 http.createServer(function (request, response) {
   response.writeHead(200, {
     'Content-Type': 'application/json; charset=utf-8'
@@ -10,8 +12,26 @@ http.createServer(function (request, response) {
 
   var request_url = url.parse(request.url);
 
-  fs.readFile('json' + request_url.pathname + '.json', function (err, data) {
-    if (err) response.end(JSON.stringify({'status': false}));
+  var pathname = request_url.pathname;
+
+  console.log('Request : ' + pathname);
+
+  fs.readFile('json' + pathname + '.json', function (err, data) {
+    if (err) {
+      if (request_url.pathname.match(universal_pattern)) {
+        pathname = request_url.pathname.replace(universal_pattern, '/$1/_all_/$2');
+
+        console.log('==> Not found, fallback to : ' + pathname + '\n');
+
+        fs.readFile('json' + pathname + '.json', function (errerr, datadata) {
+          if (errerr) response.end(JSON.stringify({'status': false}));
+          else response.end(datadata);
+        });
+      }
+      else {
+        response.end(JSON.stringify({'status': false}));
+      }
+    }
     else response.end(data);
   });
 
