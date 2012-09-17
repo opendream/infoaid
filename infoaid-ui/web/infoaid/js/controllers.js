c4b273814ee880ef80d8d6b82d33fcc6530b42d6
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-09-17 16:29:50 +0700

    add js/controllers.js

---
+++ infoaid-ui/web/infoaid/js/controllers.js
@@ -0,0 +1,28 @@
+function ListCtrl($scope, Post) {
+    var slug = $scope.slug;
+
+    var lastRowDateCreated = function () {
+        return $($scope.posts).last().get(0).dateCreated;
+    };
+
+    $scope.posts = Post.query({slug: slug});
+
+    $scope.loadMore = function () {
+        Post.query({
+            slug: slug,
+            until: lastRowDateCreated(),
+            limit: 10
+        }, function (posts) {
+            angular.forEach(posts, function (post) {
+                $scope.posts.push(post);
+            });
+        });
+    };
+}
+
+function MemberCtrl($scope, $http) {  
+    console.log('hello: '+$scope.slug);
+    $http.get(baseUrl+'/index.php/api/members/'+$scope.slug).success(function(data) {
+        $scope.members = data.topMembers;
+    });
+}
