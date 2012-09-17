98ebf5be0eab552c1a58087858de99ced069f449
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-09-17 16:58:38 +0700

    use max=infoaid.api.post.max instead of max=10

--- infoaid-api/grails-app/services/opendream/infoaid/service/HomeService.groovy
+++ infoaid-api/grails-app/services/opendream/infoaid/service/HomeService.groovy
@@ -3,15 +3,18 @@ package opendream.infoaid.service
 import opendream.infoaid.domain.Post
 
 class HomeService {
-
-    def getFeedByRecentPost(user, offset = 0, max = 10) {
+    def grailsApplication
+    
+    def getFeedByRecentPost(user, offset = 0, max = 0) {
+        max = max?:grailsApplication.config.infoaid.api.post.max
         def posts = Post.findAll(max: max, sort:"lastActived", order : 'desc', offset: offset) {
             page in user.pages
         }        
         posts
     }
 
-    def getFeedByTopPost(user, offset = 0, max = 10) {
+    def getFeedByTopPost(user, offset = 0, max = 0) {
+        max = max?:grailsApplication.config.infoaid.api.post.max
         def posts = Post.findAll(max: max, sort:"conversation", order : 'desc', offset: offset) {
             page in user.pages
         }        
