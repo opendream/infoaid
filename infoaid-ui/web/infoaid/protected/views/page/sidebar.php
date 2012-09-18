13c177d9456cffaf3f667ad867aac3d483469742
Nachapol Tongyusook <nachapol.t@gmail.com>
2012-09-18 18:45:25 +0700

    add joinus if not login

--- infoaid-ui/web/infoaid/protected/views/page/sidebar.php
+++ infoaid-ui/web/infoaid/protected/views/page/sidebar.php
@@ -1,17 +1,33 @@
 <?php
-    $resultPages = API::getJSON('user/getPages', array('id'=>8185));
-    $resultUserInfo = API::getJSON('user/showBasicInfo', array('id'=>8185));
+	$session = new CHttpSession;
+	$session->open();
+	$userId = $session['userId'];
+
+    $resultPages = API::getJSON('user/getPages', array('id'=>$userId));
+    $resultUserInfo = API::getJSON('user/showBasicInfo', array('id'=>$userId));
+
+    $session->close();
 ?>
 <div id='page-sidebar' class='span2'>
+	<div id='page-sidebar-joinus'>
+		<?php
+			if($userId == null) {
+				echo CHtml::link('Join Us',array('user/create'),array('class'=>'btn-info btn-large btn-joinus'));
+			}
+		?>
+	</div>
 	<div id='page-sidebar-profile'>
 		<div class='page-sidebar-profile'>
 			<span>
+
 				<?php
-					if($post->picSmall == null) {
-		                echo '<img src='.Yii::app()->baseUrl.'/media/profiles/profile_default_small.png class="img-polaroid">';
-		            } else {
-		                echo '<img src=' . Yii::app()->baseUrl . $resultUserInfo->picSmall . ' class="img-polaroid">';
-		            }
+					if($userId != null) {
+						if($post->picSmall == null) {
+			                echo '<img src='.Yii::app()->baseUrl.'/media/profiles/profile_default_small.png class="img-polaroid">';
+			            } else {
+			                echo '<img src=' . Yii::app()->baseUrl . $resultUserInfo->picSmall . ' class="img-polaroid">';
+			            }
+		        	}
 				?>
 			</span>
 			<span class='page-sidebar-profile-username'>
@@ -23,22 +39,50 @@
 	</div>
 	<div id='page-sidebar-menu' class='page-sidebar-menu'>
 		<ul class='unstyled ul-list'>
-			<li class='left-side-bar li-feeds'><?php echo CHtml::link('New Feeds'); ?></li>
-			<li class='left-side-bar li-private-message'><?php echo CHtml::link('Private Messages'); ?></li>
+			<li class='left-side-bar li-feeds'>
+				<?php 
+					if($userId != null) {
+						echo CHtml::link('New Feeds'); 
+					}
+				?>
+			</li>
+			<li class='left-side-bar li-private-message'>
+				<?php 
+					if($userId != null) {
+						echo CHtml::link('Private Messages'); 
+					}
+				?>
+			</li>
 
 		</ul>
 	</div>
 	<div id='page-sidebar-places'>
-		<div id='page-sidebar-places' class='page-sidebar-places-header'><p class='muted'>Place</p></div>
+		<div id='page-sidebar-places' class='page-sidebar-places-header'><p class='muted'>
+			<?php
+				if($userId != null) {
+			?>
+			Place
+			<?php
+				}
+			?>
+			</p></div>
 		<ul class='unstyled ul-list'>
 		<?php 
-			foreach($resultPages->pages as $el) {
+			if($userId != null) {
+				foreach($resultPages->pages as $el) {
 		?>
-				<li class='left-side-bar li-place'><?php echo CHtml::link($el->name, array("page/$el->slug")); ?></li>
+					<li class='left-side-bar li-place'><?php echo CHtml::link($el->name, array("page/$el->slug")); ?></li>
 		<?php
+				}
 			}
 		?>
-			<li class='left-side-bar li-add-group'><?php echo CHtml::link('Add Group..'); ?></li>
+			<li class='left-side-bar li-add-group'>
+				<?php
+					if($userId != null) { 
+						echo CHtml::link('Add Group..');
+					}
+				?>
+			</li>
 		<ul>
 	</div>
 </div>
