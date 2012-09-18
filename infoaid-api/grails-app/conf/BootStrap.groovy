4b88ba70525db4fbcca956db17d168743b028779
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-09-18 18:10:46 +0700

    add create Role add create UserRole in develop env

--- infoaid-api/grails-app/conf/BootStrap.groovy
+++ infoaid-api/grails-app/conf/BootStrap.groovy
@@ -1,4 +1,6 @@
 import opendream.infoaid.domain.User
+import opendream.infoaid.domain.Role
+import opendream.infoaid.domain.UserRole
 
 class BootStrap {
 
@@ -6,9 +8,11 @@ class BootStrap {
         development {
             if(!User.count()) {
                 println "insert user"
-                new User(username: 'admin', password: 'password', 
+                def user = new User(username: 'admin', password: 'password', 
                 firstname: 'thawatchai', lastname: 'jong', dateCreated: new Date(), 
                 lastUpdated: new Date()).save(flush:true)
+                def role = new Role(authority:'ROLE_ADMIN').save(flush:true)
+                UserRole.create(user, role)
             }
         }
     }
