21af242ca3b0cd58ef8745607ee80eeb510f1047
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-09-19 19:14:28 +0700

    add DeleteComment function add DeleteComment, Post to CommentCtrl create function refresh() to refresh posts after add new comment and delete comment

--- infoaid-ui/web/infoaid/js/main/commentService.js
+++ infoaid-ui/web/infoaid/js/main/commentService.js
@@ -7,9 +7,13 @@ angular.module('commentService', ['ngResource']).
 	factory('PostComment', function ($resource) {
 		var PostComment = $resource(baseUrl + '/api/comment');
 		return PostComment;
+	}).
+	factory('DeleteComment', function ($resource) {
+		var DeleteComment = $resource(baseUrl + '/api/deleteComment/:commentId');
+		return DeleteComment;
 	});
 
-function CommentCtrl($scope, Comment, PostComment) {
+function CommentCtrl($scope, Comment, PostComment, DeleteComment, Post) {
 
 	var lastRowLastUpdated = function () {
 		var comments = $($scope.comments);
@@ -25,7 +29,7 @@ function CommentCtrl($scope, Comment, PostComment) {
 		$scope.comments = $scope.post.comments.slice(0, 3).reverse();
 	}
 
-	$scope.loadMore = function () {
+	$scope.loadMore = function() {
 		var options = {
 				postId: $scope.post.id,
 				limit: 10 
@@ -40,10 +44,11 @@ function CommentCtrl($scope, Comment, PostComment) {
 		$scope.comments = $scope.comments || [];
 		Comment.query(options, function (comments) {
 			angular.forEach(comments, function (comment) {
-				$scope.comments.unshift(comment);
+				//$scope.comments.unshift(comment);
+				$scope.comments.push(comment);
 			});
 		});
-	};
+	}
 	
 	$scope.submitComment = function() {
 		if (this.comment && this.memberId && this.post.id) {	
@@ -53,17 +58,44 @@ function CommentCtrl($scope, Comment, PostComment) {
 				message: this.comment
 			};
 			
-			var result = PostComment.query(options, function (ret) {	
-				//console.log(ret.status);
+			PostComment.get(options, function (ret) {	
+				refresh();	
 				$scope.comment = '';		
-				
-			});					
+				var comment = {'id':ret.commentId, 'picSmall':ret.picSmall, 'createdBy':ret.user, 
+								'message':ret.comment, 'lastUpdated':ret.lastUpdated};
+				$scope.comments.push(comment);	
+				$scope.comments.splice(0, 1);				
+			});							
 		}
 	};
 	
 	$scope.deleteComment = function(comment) {
-		console.log(comment.id);	
-			
+		if(comment.id) {
+			var options = { commentId: comment.id, userId: this.memberId };
+			DeleteComment.get(options, function (ret) {	
+				refresh();			
+				for (var i = 0, ii = $scope.comments.length; i < ii; i++) {
+					if($scope.comments[i].id == ret.id) {
+						$scope.comments.splice(i, 1);
+						return;
+					}
+				}
+			});
+
+		}		
 	}
 
+	function refresh() {
+		Post.query({slug:$scope.slug}, function (posts) {					 
+			var i = 0;
+			angular.forEach(posts, function(post) {
+				if($scope.posts.length > i) {
+					$scope.posts.splice(i, 1, post);
+				} else {
+					$scope.posts.push(post);
+				}
+				i++;
+			});					
+		});
+	}
 }
