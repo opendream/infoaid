bada5a1b283cd69f88ba285469959bba4b1c29b0
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-09-20 10:15:36 +0700

    correct wording form comfirmedPassword to confirmedPassword

--- infoaid-api/test/unit/opendream/infoaid/service/UserServiceTests.groovy
+++ infoaid-api/test/unit/opendream/infoaid/service/UserServiceTests.groovy
@@ -72,7 +72,7 @@ class UserServiceTests {
 
     void testUpdatePassword() {   
         def updateparams = [id:user.id, oldPassword: 'password', 
-            newPassword: 'new-password', comfirmedPassword: 'new-password']
+            newPassword: 'new-password', confirmedPassword: 'new-password']
         def result = service.updatePassword(updateparams)        
         assert "password is updated" == result.message
 
@@ -82,21 +82,21 @@ class UserServiceTests {
 
     void testUpdatePasswordWithWrongOldPassword() {
         def updateparams = [id:user.id, oldPassword: 'passwordx', 
-            newPassword: 'new-password', comfirmedPassword: 'new-password']
+            newPassword: 'new-password', confirmedPassword: 'new-password']
         def result = service.updatePassword(updateparams)        
         assert "wrong password" == result.message
     }
 
     void testUpdatePasswordWithWrongNewPassword() {
         def updateparams = [id:user.id, oldPassword: 'password', 
-            newPassword: 'new-password', comfirmedPassword: 'new-passwordx']
+            newPassword: 'new-password', confirmedPassword: 'new-passwordx']
         def result = service.updatePassword(updateparams)        
         assert "password confirmation mismatch" == result.message
     }
 
     void testUpdatePasswordFail() {
         def updateparams = [id:user.id, oldPassword: 'password', 
-            newPassword: '', comfirmedPassword: '']  
+            newPassword: '', confirmedPassword: '']  
         def result = service.updatePassword(updateparams)
         assert "Password must have 7 to 20 character" == result.message
     }
