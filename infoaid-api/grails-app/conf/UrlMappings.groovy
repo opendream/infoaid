e71097bf80c23b8e13c8c209a97e53c5becccdd6
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-09-24 19:12:30 +0700

    map new url

--- infoaid-api/grails-app/conf/UrlMappings.groovy
+++ infoaid-api/grails-app/conf/UrlMappings.groovy
@@ -16,7 +16,7 @@ class UrlMappings {
         "/page/$slug/members"(controller: "page", action: "member")
         "/page/$slug/need"(controller: "page", action: "need")        
         "/page/$slug/post_message/$userId"(controller: "page"){ action =[POST:"postMessage"] }
-        "/page/$slug/post_need/$userId"(controller: "page"){ action =[POST:"postNeed"] }
+        "/page/$slug/post_need"(controller: "page"){ action =[POST:"postNeed"] }
         "/page/$slug/recent_post"(controller: "page", action: "recentPost")
         "/page/$slug/status"(controller: "page", action: "status") // default by recent posts
         "/page/$slug/top_members"(controller: "page", action: "topMember")
