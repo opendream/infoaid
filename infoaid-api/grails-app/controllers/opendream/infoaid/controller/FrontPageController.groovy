b5d9c00676a6c9810ba9862a03a8451c0827a5d7
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-10-10 10:52:31 +0700

    use PageSummary.findAllByHasNeed(true) instead of pageService.getActiveNeedPage()

--- infoaid-api/grails-app/controllers/opendream/infoaid/controller/FrontPageController.groovy
+++ infoaid-api/grails-app/controllers/opendream/infoaid/controller/FrontPageController.groovy
@@ -1,5 +1,6 @@
 package opendream.infoaid.controller
 
+import opendream.infoaid.domain.PageSummary
 import grails.converters.JSON
 
 class FrontPageController {
@@ -7,20 +8,21 @@ class FrontPageController {
 
     def info() {
         def ret = [:]
-        def pageNeedActive = pageService.getActiveNeedPage()
+        def pageSummary = PageSummary.findAllByHasNeed(true)
+        //def pageNeedActive = pageService.getActiveNeedPage()
         ret.status = 1
-        def pages = pageNeedActive.collect {
+        def pages = pageSummary.collect {
             [
                 name: it.name,
                 slug: it.slug,
                 lat: it.lat,
-                lng: it.lng,
-                
-                needs: pageService.getLimitNeeds(it.slug, 5).needs.collect {
-                   [
-                        item: it.item.name,
-                        quantity: it.quantity
-                   ]
+                lng: it.lng,                
+                //needs: pageService.getLimitNeeds(it.slug, 5).needs.collect {
+                need: it.items.collect {
+                    [
+                        item: it.name,
+                        quantity: it.need
+                    ]
                 }
             ]
             
