91ea44cd547c67c5af6d30e39606e744c8b03767
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-10-17 14:55:11 +0700

    Fix navigation menu link not active (Issue: #11)

--- infoaid-ui/web/infoaid/protected/views/layouts/main.php
+++ infoaid-ui/web/infoaid/protected/views/layouts/main.php
@@ -29,42 +29,60 @@
 		<div class="navbar-inner">            
 			<a class="brand" href="<?php echo Yii::app()->homeUrl ?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
 
-			<ul class="nav actions">
-				<li>
-					<a href="<?php echo $this->createUrl('/page/search'); ?>">
-						Place
-					</a>
-				</li>
-				<li><a href="#">People</a></li>
-				<li><a href="#">Status</a></li>
-				<li><a href="/site/pages/about">About us</a></li>
-			</ul>
+			<?php $this->widget('zii.widgets.CMenu', array(
+				'htmlOptions' => array(
+					'class' => 'nav actions',
+				),
+				'items' => array(
+					array('label' => 'Place', 'url' => array('/page/search')),
+					array('label' => 'People', 'url' => array('people')),
+					array('label' => 'Status', 'url' => array('status')),
+					array('label' => 'About us', 'url' => array('about')),
+				),
+			)); ?>
 
 			<?php if (Yii::app()->user->isGuest): ?>
-			<ul class="nav menu pull-right">
-					<li>
-						<a href="<?php echo $this->createUrl('/user/login'); ?>">
-							Log in
-						</a>
-					</li>
-					<li>
-						<a href="<?php echo $this->createUrl('/user/register'); ?>">
-							Register
-						</a>
-					</li>
-			</ul>
+
+			<?php $this->widget('zii.widgets.CMenu', array(
+				'htmlOptions' => array(
+					'class' => 'nav menu pull-right',
+				),
+				'items' => array(
+					array('label' => 'Log in', 'url' => array('/user/login')),
+					array('label' => 'Register', 'url' => array('/user/create')),
+				),
+			)); ?>
+
 			<?php else: ?>
-			<ul class="nav menu pull-right">
-				<li class="dropdown">
-					<a href="" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user icon-black"></i> <?php echo Yii::app()->user->getName(); ?> <b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li><a href="<?php echo $this->createUrl('user/edit'); ?>">Edit Profile</a></li>
-						<li><a href="<?php echo $this->createUrl('page/create'); ?>">Create Page</a></li>                             
-						<li class="divider"></li>
-						<li><a href="<?php echo $this->createUrl('user/logout'); ?>">Logout</a></li>
-					</ul>
-				</li>
-			</ul>
+
+			<?php $this->widget('zii.widgets.CMenu', array(
+				'htmlOptions' => array(
+					'class' => 'nav menu pull-right',
+				),
+				'encodeLabel' => false,
+				'items' => array(
+					array(
+						'label' => '<i class="icon-user icon-black"></i> '. Yii::app()->user->getName() .' <b class="caret"></b>',
+						'url' => array('user/login'),
+						'linkOptions' => array(
+							'class' => 'dropdown-toggle',
+							'data-toggle' => 'dropdown',
+						),
+						'submenuOptions' => array(
+							'class' => 'dropdown-menu',
+						),
+						'items' => array(
+							array('label' => 'Edit Profile', 'url' => '/user/edit'),
+							array('label' => 'Create Page', 'url' => '/page/create'),
+							array(
+								'template' => '<li class="divider"></li>',
+							),
+							array('label' => 'Logout', 'url' => '/user/logout'),
+						),
+					),
+				),
+			)); ?>
+			
 			<p class="navbar-text pull-right">Log in as </p>
 			<?php endif; ?>
 		</div> <!-- navbar inner -->
